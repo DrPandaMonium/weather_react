c4590547ec20521460eedf65d947adafcfd2d40c
DrPandaMonium <brianhuy.nguyen427@gmail.com>
2023-10-12 12:17:57 -0500

    added formatting and search

--- src/App.js
+++ src/App.js
@@ -1,9 +1,59 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
 const App = () => {
+    const apiKey = '29f7dbbd7470b28ced9e372d41098ace';
+    const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=imperial&q=';
+
+    const [searchTerm, setSearchTerm] = useState('');
+    const [weather, setWeather] = useState({});
+
+    async function checkWeather(city) {
+        const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+        if (response.status == 404) {
+            
+        } else {
+            let data = await response.json();
+            setWeather({
+                city: data.name,
+                temp: data.main.temp,
+                humidity: data.main.humidity,
+                wind: data.wind.speed
+            });
+            console.log(data);
+        }
+    }
+
+    useEffect(() => {
+        checkWeather('Nashville')
+    }, []);
+
+    console.log(weather);
+
     return (
-        <div>
-            Hello World
+        <div className='card'>
+            <div className='search'>
+                <input type='text' placeholder="Enter City Name" spellCheck='false' onChange={(e) => setSearchTerm(e.target.value)}></input>
+                <button onClick={() => checkWeather(searchTerm)}>Search</button>
+            </div>
+            <div className='weather'>
+                <h1 className="temp">{weather.temp}°F</h1>
+                <h2 className="city">{weather.city}</h2>
+                <div className="details">
+                    <div className="col">
+                        <div>
+                            <p className="humidity">{weather.humidity}</p>
+                            <p>Humidity</p>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div>
+                            <p className="wind">{weather.wind}kph</p>
+                            <p>Wind Speed</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
